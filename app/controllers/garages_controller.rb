34488bf4ff80cff5736a677025faf73a4c9aef37
Young Koh <youngheon6@gmail.com>
2023-03-22 11:48:18 -0600

    feat: Test and implement User Story 1

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,5 +1,5 @@
 class GaragesController < ApplicationController
   def index
-    
+    @garages = Garage.all
   end
 end
--- spec/features/garage/index_spec.rb
+++ spec/features/garage/index_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe "/garage", type: :feature do
   #User Story 1
   describe "as a visitor, when i visit the garage index page" do
+    let!(:garage_1){Garage.create!(indoor: true, slots: 300, city:"Denver", zipcode:"80032", name:"Cherry Creek Garage")}
+    let!(:garage_2){Garage.create!(indoor: true, slots: 512, city:"Denver", zipcode:"80234", name:"Lakewood Garage")}
+    let!(:garage_3){Garage.create!(indoor: false, slots: 800, city:"Boulder", zipcode:"81032", name:"Boulder Garage")}
+
     it "displays the name of each garage" do
       visit "/garages"
 
+      # save_and_open_page
       expect(page).to have_content(garage_1.name)
       expect(page).to have_content(garage_2.name)
       expect(page).to have_content(garage_3.name)
