4ba8f4f5816bbe0f9bc40b2794b1c2dbacc76cc7
Young Koh <youngheon6@gmail.com>
2023-03-27 16:51:43 -0600

    feat: Test and implement User Story 23

--- spec/features/cars/index_spec.rb
+++ spec/features/cars/index_spec.rb
@@ -81,4 +81,61 @@ RSpec.describe "/cars", type: :feature do
       expect(page).to have_current_path("/cars/#{car_3.id}/edit")
     end
   end
+
+  #User Story 23 pt.1
+  describe "as a visitor, when i visit /cars" do
+    let!(:garage_1){Garage.create!(indoor: true, slots: 300, city:"Denver", zipcode:"80032", name:"Cherry Creek Garage")}
+    let!(:car_1){garage_1.cars.create!(operational: true, miles: 44523, color: "blue", owner: "Adam")}
+    let!(:car_2){garage_1.cars.create!(operational: true, miles: 14093, color: "black", owner: "Gregor")}
+    let!(:car_3){garage_1.cars.create!(operational: true, miles: 204011, color: "orange", owner: "Sandor")}
+
+    it " displays a link next to each car,'Delete Car: id'" do
+      visit "/cars"
+      expect(page).to have_content("Owner: #{car_1.owner}")
+      expect(page).to have_content("Miles: #{car_1.miles}")
+      expect(page).to have_selector(:button, "Delete Car: #{car_1.id}")
+
+      expect(page).to have_content("Owner: #{car_2.owner}")
+      expect(page).to have_content("Miles: #{car_2.miles}")
+      expect(page).to have_selector(:button, "Delete Car: #{car_2.id}")
+
+      expect(page).to have_content("Owner: #{car_3.owner}")
+      expect(page).to have_content("Miles: #{car_3.miles}")
+      expect(page).to have_selector(:button, "Delete Car: #{car_3.id}")
+    end
+
+    it "clicking the link deletes the car, redirects to /cars, and displays updated records" do
+      visit "/cars"
+      # save_and_open_page
+      click_button("Delete Car: #{car_3.id}")
+      expect(page).to have_current_path("/cars/")
+
+      expect(page).to have_content("Owner: #{car_1.owner}")
+      expect(page).to have_content("Miles: #{car_1.miles}")
+      expect(page).to have_selector(:button, "Delete Car: #{car_1.id}")
+
+      expect(page).to have_content("Owner: #{car_2.owner}")
+      expect(page).to have_content("Miles: #{car_2.miles}")
+      expect(page).to have_selector(:button, "Delete Car: #{car_2.id}")
+
+      expect(page).to_not have_content("Owner: #{car_3.owner}")
+      expect(page).to_not have_content("Miles: #{car_3.miles}")
+      expect(page).to_not have_selector(:button, "Delete Car: #{car_3.id}")
+
+      click_button("Delete Car: #{car_2.id}")
+      expect(page).to have_current_path("/cars/")
+
+      expect(page).to have_content("Owner: #{car_1.owner}")
+      expect(page).to have_content("Miles: #{car_1.miles}")
+      expect(page).to have_selector(:button, "Delete Car: #{car_1.id}")
+
+      expect(page).to_not have_content("Owner: #{car_2.owner}")
+      expect(page).to_not have_content("Miles: #{car_2.miles}")
+      expect(page).to_not have_selector(:button, "Delete Car: #{car_2.id}")
+
+      expect(page).to_not have_content("Owner: #{car_3.owner}")
+      expect(page).to_not have_content("Miles: #{car_3.miles}")
+      expect(page).to_not have_selector(:button, "Delete Car: #{car_3.id}")
+    end
+  end
 end
